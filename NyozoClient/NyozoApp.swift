fea0a608c2298008ff2c7726d9262fcfd84f4901
naonya3 <naoto.horiguchi@gmail.com>
2017-09-03 00:19:06 +0900

    Touchbarを使うにあたっての準備など

--- NyozoClient/NyozoApp.swift
+++ NyozoClient/NyozoApp.swift
@@ -17,4 +17,35 @@ class NyozoApp: NSApplication {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    @available(OSX 10.12.2, *)
+    override func makeTouchBar() -> NSTouchBar? {
+        let touchBar = NSTouchBar()
+        touchBar.delegate = self
+        touchBar.defaultItemIdentifiers = [.service]
+        return touchBar
+    }
+}
+
+fileprivate extension NSTouchBarItemIdentifier {
+    static let service = NSTouchBarItemIdentifier("me.naonya3.NyozoClient.touchbar.service")
+}
+
+extension NyozoApp: NSTouchBarDelegate {
+    
+    @available(OSX 10.12.2, *)
+    func touchBar(_ touchBar: NSTouchBar, makeItemForIdentifier identifier: NSTouchBarItemIdentifier) -> NSTouchBarItem? {
+        switch identifier {
+        case NSTouchBarItemIdentifier.service:
+            let custom = NSCustomTouchBarItem(identifier: .service)
+            custom.customizationLabel = "TouchBar Catalog Label"
+            let label = NSTextField.init(labelWithString: "Catalog")
+            custom.view = label
+            return custom
+        default:
+            return nil
+        }
+    }
+    
+    
 }
